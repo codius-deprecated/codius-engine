544dae0e30f7cab97af980b105cbef796a2f6971
Evan Schwartz <evan.mark.schwartz@gmail.com>
2014-07-02 16:15:30 -0700

    [TEST] Add more complex require test

--- test/runtime_library.module.js
+++ test/runtime_library.module.js
@@ -18,47 +18,30 @@ describe('Runtime Library module', function(){
   describe('require', function(){
 
     it('should respond with an error if the identifier is neither a module, nor a javascript or JSON file', function(){
-      var __readFileSync = sinon.stub();
-      __readFileSync.returns('{"test":[1,2],"a":"b","c":{"d":-1}}');
-      var context = {
-        __readFileSync: __readFileSync
-      };
+      var context = { __readFileSync: sinon.stub() };
       var module = getNewModuleVersion(context);
-
       expect(function(){ module.require('./test.txt'); }).to.throw(/require can only be used to load modules, javascript files, and JSON files/);
     });
 
-    it('should call __readFileSync with the path', function(){
-
-      var __readFileSync = sinon.spy();
-      var context = {
-        __readFileSync: __readFileSync
-      };
+    it('should call __readFileSync with the module_identifier', function(){
+      var context = { __readFileSync: sinon.spy() };
       var module = getNewModuleVersion(context);
-
       var test_module = module.require('module_id');
-      expect(__readFileSync).to.have.been.calledOnce;
+      expect(context.__readFileSync).to.have.been.calledOnce;
+      expect(context.__readFileSync.firstCall.args).to.deep.equal(['module_id']);
     });
 
     it('should eval and extract the module.exports from javascript files', function(){
-
-      var __readFileSync = sinon.stub();
-      __readFileSync.returns('module.exports={ global_data: "Hello World!" }');
-      var context = {
-        __readFileSync: __readFileSync
-      };
+      var context = { __readFileSync: sinon.stub() };
+      context.__readFileSync.returns('module.exports={ global_data: "Hello World!" }');
       var module = getNewModuleVersion(context);
-
       var test_module = module.require('module_id');
       expect(test_module).to.have.property('global_data', 'Hello World!');
     });
 
     it('should parse json files', function(){
-      var __readFileSync = sinon.stub();
-      __readFileSync.returns('{"test":[1,2],"a":"b","c":{"d":-1}}');
-      var context = {
-        __readFileSync: __readFileSync
-      };
+      var context = { __readFileSync: sinon.stub() };
+      context.__readFileSync.returns('{"test":[1,2],"a":"b","c":{"d":-1}}');
       var module = getNewModuleVersion(context);
 
       var test_module = module.require('test.json');
@@ -70,20 +53,25 @@ describe('Runtime Library module', function(){
     });
 
     it('should expose a modified version of itself to submodules that translates relative paths to full paths', function(){
-
-      var __readFileSync = sinon.stub();
-      __readFileSync.onFirstCall().returns('module.exports = require("sub_module");');
-      __readFileSync.onSecondCall().returns('module.exports={ global_data: "Hello World!" }');
-      var context = {
-        __readFileSync: __readFileSync
-      };
+      var context = { __readFileSync: sinon.stub() };
+      context.__readFileSync.onFirstCall().returns('module.exports = require("sub_module");');
+      context.__readFileSync.onSecondCall().returns('module.exports={ global_data: "Hello World!" }');
       var module = getNewModuleVersion(context);
-
       var test_module = module.require('top_module');
 
       expect(test_module).to.have.property('global_data', 'Hello World!');
-      expect(__readFileSync.secondCall.args).to.deep.equal([ 'top_module/contract_modules/sub_module' ]);
+      expect(context.__readFileSync.secondCall.args).to.deep.equal([ 'top_module/contract_modules/sub_module' ]);
+    });
 
+    it.only('should call __readFileSync with full paths that are normalizeable by the Node.js path module', function(){
+      var context = { __readFileSync: sinon.stub(), console: { log: console.log } };
+      context.__readFileSync.onFirstCall().returns('require("sub_module");');
+      context.__readFileSync.onSecondCall().returns('require("./contract_modules/sub_sub_module/somefile.js");');
+      // context.__readFileSync.onThirdCall().returns('module.exports={ global_data: "Hello World!" }');
+      var module = getNewModuleVersion(context);
+      var test_module = module.require('top_module');
+
+      expect(context.__readFileSync.thirdCall.args).to.deep.equal([ 'top_module/contract_modules/sub_module/contract_modules/sub_sub_module/somefile.js' ]);
     });
 
 
