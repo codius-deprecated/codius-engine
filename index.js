aa533212ac2056efec4876b63bf31ff33c95ef0f
Stefan Thomas <justmoon@members.fsf.org>
2014-07-05 00:03:40 -0700

    [TASK] Refactor into object-oriented architecture.
    Allows engine to be used as a library.
    Split compilation from file management (compiler can be used by CLI to generate files for upload.)

---
+++ index.js
@@ -0,0 +1,4 @@
+exports.Compiler = require('./lib/compiler').Compiler;
+exports.Config = require('./lib/config').Config;
+exports.Engine = require('./lib/engine').Engine;
+exports.FileManager = require('./lib/filemanager').FileManager;
--- run-test-contract.js
+++ run-test-contract.js
@@ -1,19 +1,30 @@
 var fs = require('fs');
 var async    = require('async');
 var crypto   = require('crypto');
-var Engine   = require('./engine');
-var compile  = require('./compile-contract');
+var Config   = require('./lib/config').Config;
+var Engine   = require('./lib/engine').Engine;
+var Compiler = require('./lib/compiler').Compiler;
+var FileManager = require('./lib/filemanager').FileManager;
 
 var CONTRACT_DIR = __dirname + '/test_contract/';
 var FILESYSTEM_DIR = __dirname + '/contract_filesystem/';
 
+var config = new Config();
+config.contractsFilesystemPath = FILESYSTEM_DIR;
 
-var contract_hash = compile(CONTRACT_DIR, FILESYSTEM_DIR);
+var compiler = new Compiler(config);
+var fileManager = new FileManager(config);
+
+compiler.on('file', function (event) {
+  fileManager.storeFileWithHash(event.hash, event.data);
+});
+
+var contract_hash = compiler.compileModule(CONTRACT_DIR);
 
 /**
  *  Create a new Engine and run the test contract
  */
-var engine = new Engine();
+var engine = new Engine(config);
 console.log('Run contract: ' + contract_hash);
 engine.runContract(contract_hash, function(error, result){
   console.log('final error:', error, 'result:', result);
