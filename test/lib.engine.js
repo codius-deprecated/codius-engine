881328d2299771ed964835a4915d105373e0f124
Torrie Fischer <torrie@ripple.com>
2015-02-24 12:22:36 -0800

    Dont run a contract until weve written all of its files
    [Fixes #89035332]

--- test/lib.engine.js
+++ test/lib.engine.js
@@ -1,12 +1,14 @@
 var chai      = require('chai');
 var expect    = chai.expect;
 var sinonChai = require('sinon-chai');
+var Promise = require('bluebird').Promise;
 chai.use(sinonChai);
 
 var path      = require('path');
 var Engine    = require(path.join(__dirname, '/../lib/engine')).Engine;
 var Compiler  = require(path.join(__dirname, '/../lib/compiler')).Compiler;
 var Config    = require(path.join(__dirname, '/../lib/config')).Config;
+var FileManager = require(path.join(__dirname, '/../lib/filemanager')).FileManager;
 
 var config    = new Config();
 var engine    = new Engine(config);
@@ -25,10 +27,21 @@ describe('lib/engine', function() {
 
   describe('runContract', function(){
     it('should run a contract', function() {
+      var fileManager = new FileManager(config);
       var compiler = new Compiler(config);
+      var currentDir = path.join(__dirname, '/test_contract');
+      var p = [];
+      compiler.on('file', function(event) {
+        if (event.name.indexOf(currentDir) !== 0) {
+          throw new Error('File path does not have current directory prefix: ' + event.name);
+        }
+        p.push(fileManager.storeFileWithHash(event.hash, event.data));
+      });
       var manifestHash = compiler.compileModule(path.join(__dirname, '/test_contract'));
-      var runner = engine.runContract(manifestHash);
-      expect(runner._manifest_hash).to.equal(manifestHash)
+      Promise.all(p).then(function() {
+        var runner = engine.runContract(manifestHash);
+        expect(runner._manifest_hash).to.equal(manifestHash)
+      });
       // TODO: more tests
     });
   });
